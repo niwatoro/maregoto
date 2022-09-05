9aeb896854411ec684c174dd60b3282340e89aa3
niwatoro <girlwithskyhairpin@gmail.com>
2022-09-05 23:46:36 +0900

    create an api to wikipedia but with CORS error

--- pages/reading.js
+++ pages/reading.js
@@ -1,5 +1,46 @@
+import React, {useState,useEffect} from "react"
+import {
+    Box,
+    Spinner,
+    Center
+} from "@chakra-ui/react"
+
 export default function Reading() {
+    const [data, setData] = useState(null)
+    const [isLoading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        fetch("https://zh.wikipedia.org/w/api.php?action=query&prop=extracts&titles=next.js&explaintext")
+            .then((response) => response.json())
+            .then((data) => {
+                setData(data)
+                setLoading(false)
+            })
+    }, [])
+
+    if (isLoading) {
+        return (
+            <Box>
+                <Center>
+                    <Spinner
+                        size="xl"
+                        color="blue.500" />
+                </Center>
+            </Box>
+        )
+    }
+    if (!data) {
+        return (
+            <Box>No Data</Box>
+        )
+    }
+
+    const text = data.query.pages[0].extract
+
     return (
-        <p>reading</p>
+        <Box>
+            {data.text}
+        </Box>
     )
 }
