662a9ccd8dbb5dafe70dcde9d47836c64f44ae1d
niwatoro <girlwithskyhairpin@gmail.com>
2022-09-07 11:39:06 +0900

    minor changes on toasts

--- pages/reading.js
+++ pages/reading.js
@@ -64,7 +64,7 @@ export default function Reading() {
                                             onClick={() => {
                                                 toast.closeAll()
                                                 toast({
-                                                    title: pinyin(elem)
+                                                    title: elem + ": " + pinyin(elem)
                                                 })
                                             }}>
                                             {elem}
